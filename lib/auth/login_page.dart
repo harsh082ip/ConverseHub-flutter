c21efa69e08b4f8818bba3f9bfb40fbb83e78c2f
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-06-09 00:13:48 +0530

    Integrated Firebase in the project

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:converse_hub/main.dart';
 import 'package:flutter/material.dart';
 
@@ -12,44 +13,55 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: const Text(
-          'Welcome to Converse Hub',
-          style: TextStyle(fontWeight: FontWeight.bold),
+        title: SlideInDown(
+          delay: Duration(milliseconds: 200 * 2),
+          child: const Text(
+            'Welcome to Converse Hub',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
         backgroundColor: loginBackgroundColor,
       ),
       backgroundColor: loginBackgroundColor,
       body: Stack(children: [
-        Image.asset(
-          'assets/images/login_img.jpg',
-          height: mq.height,
-          width: mq.width,
+        ZoomIn(
+          delay: Duration(milliseconds: 200),
+          child: Image.asset(
+            'assets/images/login_img.jpg',
+            height: mq.height,
+            width: mq.width,
+          ),
         ),
         Positioned(
           bottom: mq.height * 0.1,
           left: mq.width * .05,
           width: mq.width * .9,
           height: mq.height * .07,
-          child: ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: googleButtonIconColor, elevation: 1),
-            onPressed: () {},
-            icon: Image.asset(
-              'assets/images/google.png',
-              height: 45.0,
+          child: BounceInUp(
+            delay: Duration(milliseconds: 400 * 2),
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: googleButtonIconColor, elevation: 1),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+              icon: Image.asset(
+                'assets/images/google.png',
+                height: 45.0,
+              ),
+              label: RichText(
+                  text: TextSpan(children: [
+                TextSpan(
+                    text: 'Sign in with',
+                    style: TextStyle(color: Colors.black, fontSize: 18)),
+                TextSpan(
+                    text: ' Google',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold))
+              ])),
             ),
-            label: RichText(
-                text: TextSpan(children: [
-              TextSpan(
-                  text: 'Sign in with',
-                  style: TextStyle(color: Colors.black, fontSize: 18)),
-              TextSpan(
-                  text: ' Google',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold))
-            ])),
           ),
         )
       ]),
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:converse_hub/auth/login_page.dart';
 import 'package:converse_hub/constants/consts.dart';
-
+import 'package:converse_hub/screens/home_page.dart';
 import 'package:flutter/material.dart';
+// firebase packages
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 // variable to initialize mediaQuery
 late Size mq;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  _initializeFireBase();
   runApp(const MyApp());
 }
 
@@ -24,7 +29,17 @@ class MyApp extends StatelessWidget {
             titleTextStyle: TextStyle(color: Colors.white, fontSize: 25.0)),
       ),
       debugShowCheckedModeBanner: false,
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+      },
       home: LoginPage(),
     );
   }
+}
+
+_initializeFireBase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
