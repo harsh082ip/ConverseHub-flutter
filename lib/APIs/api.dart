21a490a998d63a5a1f0e19bc9f18a53ac80e341f
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-06-10 00:12:31 +0530

    Integrated cloud_firestore and created a databse on it, also loaded data from StreamBuilder

--- lib/APIs/api.dart
+++ lib/APIs/api.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class APIs {
+  // initializing firebase
   static FirebaseAuth auth = FirebaseAuth.instance;
+
+  // for accessing firestore cloud
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:converse_hub/APIs/api.dart';
 import 'package:converse_hub/main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -16,37 +19,46 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     mq = MediaQuery.of(context).size;
     return Scaffold(
-      //appBar
-      appBar: AppBar(
-        leading: const Icon(CupertinoIcons.home),
-        title: const Text('Converse Hub'),
-        actions: [
-          //search iconButton
-          IconButton(
-              onPressed: () {
-                print('pressed');
-              },
-              icon: Icon(Icons.search)),
-          //search vertical dot iconButton
-          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-        ],
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(
-          Icons.add_comment_rounded,
-          color: Color.fromARGB(255, 107, 93, 235),
+        //appBar
+        appBar: AppBar(
+          leading: const Icon(CupertinoIcons.home),
+          title: const Text('Converse Hub'),
+          actions: [
+            //search iconButton
+            IconButton(
+                onPressed: () {
+                  print('pressed');
+                },
+                icon: Icon(Icons.search)),
+            //search vertical dot iconButton
+            IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+          ],
         ),
-      ),
-
-      body: ListView.builder(
-          physics: BouncingScrollPhysics(),
-          padding: EdgeInsets.only(top: mq.height * 0.01),
-          itemCount: 15,
-          itemBuilder: (context, index) {
-            return ChatUserCards();
-          }),
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(
+            Icons.add_comment_rounded,
+            color: Color.fromARGB(255, 107, 93, 235),
+          ),
+        ),
+        body: StreamBuilder(
+          // reading and building from the data getting from firestore cloud
+          stream: APIs.firestore.collection('users').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final data = snapshot.data?.docs;
+              for (var i in data!) {
+                print('Data: ${i.data()}');
+              }
+            }
+            return ListView.builder(
+                physics: BouncingScrollPhysics(),
+                padding: EdgeInsets.only(top: mq.height * 0.01),
+                itemCount: 15,
+                itemBuilder: (context, index) {
+                  return ChatUserCards();
+                });
+          },
+        ));
   }
 }
